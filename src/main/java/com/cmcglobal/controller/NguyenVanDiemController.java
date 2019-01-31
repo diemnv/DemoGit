9237cccd31069db54b3d155f619d1bd58c27f0d6
dinhthuan <dinhthuanvan@gmail.com>
2019-01-31 11:58:49 +0700

    no message

--- src/main/java/com/cmcglobal/controller/NguyenVanDiemController.java
+++ src/main/java/com/cmcglobal/controller/NguyenVanDiemController.java
@@ -3,5 +3,6 @@ package com.cmcglobal.controller;
 public class NguyenVanDiemController {
 	public void haoDien() {
 		System.out.println("Toi la hao, Toi bi dien");
+		System.out.println("thuan");
 	}
 }
