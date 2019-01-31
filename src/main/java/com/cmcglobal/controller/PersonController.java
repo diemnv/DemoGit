797b1bfed91540d5132666b6a9e258b49296818f
haohus <phamhao96.hus@gmail.com>
2019-01-31 11:51:53 +0700

    hao commit

---
+++ src/main/java/com/cmcglobal/controller/PersonController.java
@@ -0,0 +1,11 @@
+package com.cmcglobal.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+	public void getAll() {
+		
+	}
+
+}
